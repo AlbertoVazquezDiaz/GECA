f8351c127e36b7684af3fea3abdff83f7c2e8bf0
Alberto Vázquez Díaz <20223tn083@utez.edu.mx>
2025-05-19 18:22:59 -0600

    First commit

---
+++ client/GECA/pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="welcome-container">
+    <h1>¡Bienvenido a GECA!</h1>
+    <p>Tu plataforma para gestionar proyectos con Vue y Nuxt.</p>
+  </div>
+</template>
+
+<style scoped>
+.welcome-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 60vh;
+  text-align: center;
+}
+h1 {
+  color: #2c3e50;
+  margin-bottom: 0.5em;
+}
+p {
+  color: #606060;
+}
+</style>
